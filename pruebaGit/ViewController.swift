b6d3b6f83a301c102fb2a339373038ea906de4fd
maytepape <mayte.pavon@gmail.com>
2015-11-03 20:41:26 +0100

    Initial Commit

---
+++ pruebaGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pruebaGit
+//
+//  Created by Mayte Pavón Pedraza on 3/11/15.
+//  Copyright (c) 2015 Mayte Pavón Pedraza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
